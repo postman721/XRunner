2dfc955774cf24d462ef29a859c45a8131217a94
JJ Posti <jjposti@techtimejourney.net>
2017-09-21 00:45:43 +0300

    program upload.

---
+++ XRunner/src/xrunner/XRunner.java
@@ -0,0 +1,18 @@
+/*
+XRunner v.1 Copyright (c) 2017 JJ Posti <techtimejourney.net> This program comes with ABSOLUTELY NO WARRANTY; 
+for details see: http://www.gnu.org/copyleft/gpl.html. This is free software, and you are welcome to redistribute it under 
+GPL Version 2, June 1991")
+ */
+package xrunner;
+/*
+  @author JJ Posti -techtimejourney.net.
+ */
+public class XRunner {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    guiForm gui = new guiForm();
+        gui.setVisible(true);
+        System.out.println("XRunner starts.");}    
+}
